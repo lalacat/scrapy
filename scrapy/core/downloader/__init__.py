08b87fef910014426b685e3dd45fd166ade0a16b
scott <scott.si@hotmail.com>
2018-08-30 15:09:29 +0800

    学习urlparse

--- scrapy/core/downloader/__init__.py
+++ scrapy/core/downloader/__init__.py
@@ -110,6 +110,7 @@ class Downloader(object):
             return response
 
         self.active.add(request)
+        #  调用中间件管理器的download方法，同时传入了自己的_enqueue_request方法。
         dfd = self.middleware.download(self._enqueue_request, request, spider)
         return dfd.addBoth(_deactivate)
 
